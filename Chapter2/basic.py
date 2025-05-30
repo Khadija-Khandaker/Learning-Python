8c7ea63740683d87e8d609bbe5a4793fa0196378
Khadija-Khandaker <152651964+Khadija-Khandaker@users.noreply.github.com>
2025-05-31 00:49:33 +0600

    Add files via upload

---
+++ Chapter2/basic.py
@@ -0,0 +1,50 @@
+#Data types
+a= 1        #a is an integer
+b= 7.3      #b is a floating point
+c= "Hello"  #c is a string
+d= False    #d is a boolean variable
+e= None     #e is a none type variable
+
+# Rules of variables
+a= "Hello"
+aa= "Hello"
+value= 66
+_value= 77
+# @value= 12, invalid due to @ symbol
+# sa@meer = 12, invalid due to @ symbol
+
+# Arithmetic Operators
+a= 5
+b= 7
+c= a+b
+print(c)
+
+# Assignment Operators
+a= 4-2 #Assign 4-2 in a
+print(a)
+b= 6
+b+= 3 # Increment the value of b by 3 and then assign it to b
+b-= 2 # Decrement the value of b by 3 and then assign it to b
+print(b)
+
+# Comparison Operators
+d= 5<4
+print(d)
+e= 5>=4
+print(e)
+# d= 5==5, True
+# d= 5!=5, False
+
+#Logical Operators
+f= True or False
+print(f)
+
+print("True and False is", True and False)
+print(not(True))
+
+#Type() function and typecasting
+g= "33.3"
+h= float(g) # g but the type should be float
+t= type(h)
+print(t)
+
---
+++ Chapter2/input.py
@@ -0,0 +1,17 @@
+a= input("Enter number 1: ")
+b= input("Enter number 2: ")
+
+print("Number a is ", a)
+print("Number b is ", b)
+
+print("Sum = ", a+b) #This concanates the numbers because the input is taken in the string form
+
+
+# Sum of two numbers
+c= int(input("Enter number 1: "))
+d= int(input("Enter number 2: "))
+
+print("Number a is ", c)
+print("Number b is ", d)
+
+print("Sum = ", c+d)
